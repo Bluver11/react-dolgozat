0a411d0d756405395e02f64896f9645a71132e0d
Petrik Diak <diak@petrik.hu>
2024-02-12 12:27:15 +0100

    Program

---
+++ src/Comment.tsx
@@ -0,0 +1,7 @@
+export interface Comment{
+    id: number
+    email: string
+    avatar: string
+    content: string
+
+}
---
+++ src/Component/CommentBox.tsx
@@ -0,0 +1,15 @@
+
+
+export function CommentBox(props:Props){
+    <div>
+        <img src="{comment.avatar}" alt="" />
+    </div>
+
+}
+
+
+
+
+interface Props{
+    comment: Comment
+}
